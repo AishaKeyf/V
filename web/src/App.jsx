5deae575a7fff452ccb35c606f0fcbee51e1a825
Aisha (sarajohn786) <bilandaahir77@gmail.com>
2025-02-02 10:21:57 +0300

    react setup

---
+++ web/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, version } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className=" text-5xl font-bold">
+        <h1>Welcome to <span>V</span></h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
